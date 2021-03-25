b1463e3c851b36ff21ed4d42ba0b2f21a6b9695f
tokyo-walker <nana7mi3goo@gmail.com>
2021-03-25 19:18:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <RouterView />
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
 
 <style>
 #app {
---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="checkbox-pink">
+        hoge
+        <input type="checkbox" id="modal-sign" value="pink" v-model="checkbox">
+        <label for="modal-sign">pink</label>
+    </div>
+</template>
+
+<script lang="ts">
+    import {ref, reactive, defineComponent} from 'vue'
+
+
+    export default defineComponent({
+        name: 'ModalComponent',
+        props: {
+            msg: {
+                type: String,
+                required: true,
+            },
+        },
+        setup: () => {
+            let checkbox = reactive([])
+
+            return {checkbox}
+        },
+    })
+</script>
